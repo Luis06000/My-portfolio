5f01a9883ee41e3becfeb1a52ef0eeb095f68602
Luis06000 <luisnice2005@gmail.com>
2024-11-04 16:24:35 +0100

    readme

--- src/Pages/About/Tools/Tools.js
+++ src/Pages/About/Tools/Tools.js
@@ -2,21 +2,21 @@ import './Tools.css';
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { FaWindows, FaLinux, FaGithub } from "react-icons/fa";
-import { SiVisualstudiocode, SiPhpmyadmin } from "react-icons/si";
+import { SiVisualstudiocode, SiPhpmyadmin, SiArduino } from "react-icons/si";
 
 function Tools() {
   return (
     <Row style={{
         display: "grid",
         gridTemplateColumns: "repeat(5, 1fr)",
-        marginLeft: "10em",
-        marginRight: "10em",
+        margin: "0 10em",
         gap: "2em",
         justifyContent: "center",
         alignItems: "center",
+        textAlign: "center",
         paddingBottom: "50px"
       }}>
-      <Col xs={4} md={2} className="tech-icons" title='Windoxs'>
+      <Col xs={4} md={2} className="tech-icons" title='Windows'>
         <FaWindows />
       </Col>
       <Col xs={4} md={2} className="tech-icons" title='Linux'>
@@ -30,6 +30,9 @@ function Tools() {
       </Col>
       <Col xs={4} md={2} className="tech-icons" title='php MyAdmin'>
         <SiPhpmyadmin />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons" title='Arduino'>
+        <SiArduino />
       </Col>
     </Row>
   );
--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -1,5 +1,61 @@
+// import React, { useEffect, useState } from 'react';
+// import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+// import './App.css';
+// import AppHeader from './Header/App-header.js';
+// import AppFooter from './Footer/App-footer.js';
+// import Home from '../Home/Home.js';
+// import Projects from '../Projects/Projects.js';
+// import About from '../About/About.js';
+// import Background from '../../Background/Background.js';
+// import Preloader from '../Loader/Preloader.js';
+
+// function ScrollToTop() {
+//   const { pathname } = useLocation();
+
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
+//   }, [pathname]);
+
+//   return null;
+// }
+
+// function App() {
+//   const [load, upadateLoad] = useState(true);
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       upadateLoad(false);
+//     }, 1200);
+
+//     return () => clearTimeout(timer);
+//   }, []);
+
+//   return (
+//     <Router>
+//       <Preloader load={load} />
+//       <div className="App" id={load ? "no-scroll" : "scroll"}>
+//         <Background />
+//         <AppHeader />
+//         <ScrollToTop />
+//         <Routes>
+//           <Route path="/Home" element={<Home />} />
+//           <Route path="/projects" element={<Projects />} />
+//           <Route path="/about" element={<About />} />
+//         </Routes>
+//         <AppFooter />
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from 'react-router-dom';
 import './App.css';
 import AppHeader from './Header/App-header.js';
 import AppFooter from './Footer/App-footer.js';
@@ -20,11 +76,11 @@ function ScrollToTop() {
 }
 
 function App() {
-  const [load, upadateLoad] = useState(true);
+  const [load, updateLoad] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      upadateLoad(false);
+      updateLoad(false);
     }, 1200);
 
     return () => clearTimeout(timer);
@@ -38,7 +94,8 @@ function App() {
         <AppHeader />
         <ScrollToTop />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Navigate to="/Home" />} />
+          <Route path="/Home" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
         </Routes>
